4a8e128903e759fdaeeced7555d0fffdb6198b1d
Furkan Coskun <furkancoskunn96@gmail.com>
2022-04-24 01:53:56 +0300

    DBProvider DBProvider Non-Generic bir hale getirildi ve DAL katmanı içerisinde taşındı.

---
+++ SS.DataAccessLayer/Concrete/DBProvider.cs
@@ -0,0 +1,21 @@
+﻿using SS.DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS.DataAccessLayer.Concrete
+{
+    public static class DBProvider
+    {
+        private static string _connectionString;
+
+        private static IDataAccess _dataAccess = new DataAccess();
+
+        public static string connectionString { get {return _connectionString; } set { _connectionString = value; } }
+
+        public static IDataAccess DB { get { return _dataAccess; } set { _dataAccess = value; } }
+    }
+}
