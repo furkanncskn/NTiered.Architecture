a791a401a54709ba2717a534416ab43fff3cb1f5
Furkan Coskun <furkancoskunn96@gmail.com>
2022-04-24 18:55:48 +0300

    Add Form Eklendi

---
+++ SS.WinForm.UI/Forms/Add.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace SS.WinForm.UI.Forms
+{
+    partial class Add
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.usersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.usersBindingSource, "USER_NAME", true));
+            this.txtName.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(16, 52);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(345, 30);
+            this.txtName.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "İSİM";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.usersBindingSource, "USER_PASSWORD", true));
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(16, 127);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(345, 30);
+            this.txtPassword.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "PASSWORD";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.usersBindingSource, "USER_EMAIL", true));
+            this.txtEmail.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(16, 208);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(345, 30);
+            this.txtEmail.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 23);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "EMAİL";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAdd.Location = new System.Drawing.Point(94, 274);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(178, 42);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "KAYDET";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // usersBindingSource
+            // 
+            this.usersBindingSource.DataSource = typeof(Entity.Concrete.Users);
+            // 
+            // Add
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 363);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Add";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add";
+            this.Load += new System.EventHandler(this.Add_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.BindingSource usersBindingSource;
+    }
+}
---
+++ SS.WinForm.UI/Forms/Add.cs
@@ -0,0 +1,75 @@
+﻿using Entity.Concrete;
+using SS.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SS.WinForm.UI.Forms
+{
+    public partial class Add : Form
+    {
+        public Add()
+        {
+            InitializeComponent();
+        }
+
+        #region EventHandlers
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            #region VALIDATION
+            usersBindingSource.EndEdit();
+
+            Users user = usersBindingSource.DataSource as Users;
+            
+            ValidationContext context = new ValidationContext(user);
+            
+            ICollection<ValidationResult> results = new List<ValidationResult>();
+            
+            if(!Validator.TryValidateObject(user,context,results))
+            {
+                foreach (var item in results)
+                {
+                    MessageBox.Show(
+                            "Error Message: " + item.ErrorMessage, 
+                            "Message", 
+                            MessageBoxButtons.OK, 
+                            MessageBoxIcon.Warning
+                        );
+                }
+
+                return;
+            };
+            #endregion // VALIDATION
+
+            #region ADD
+            SqlCommand command = new SqlCommand()
+            {
+                CommandText = "sp_insert_Users",
+                CommandType = CommandType.StoredProcedure
+            };
+
+            command.AddSqlParameter("USER_NAME", ParameterDirection.Input, SqlDbType.VarChar, txtName.Text ,150);
+            command.AddSqlParameter("USER_PASSWORD", ParameterDirection.Input, SqlDbType.VarChar, txtPassword.Text, 150);
+            command.AddSqlParameter("USER_EMAIL", ParameterDirection.Input, SqlDbType.VarChar, txtEmail.Text, 150);
+
+            int affectedRow = DBProvider.DB.NonQueryCommand(command, DBProvider.connectionString);
+            #endregion // ADD
+
+            #region ERR_MSG
+            if (!(affectedRow > 0))
+                MessageBox.Show("Kayıt işlemi başarısız oldu", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("Tebrikler, kayıt başarı ile gerçekleşti.\nAilemize Hoşgeldiniz :)", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            #endregion //ERR_MSG
+        }
+
+        private void Add_Load(object sender, EventArgs e)
+        {
+            usersBindingSource.DataSource = new Users();
+        }
+        #endregion // EventHandlers
+    }
+}
