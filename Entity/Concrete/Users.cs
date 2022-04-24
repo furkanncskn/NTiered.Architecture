4b6190d03bebc17eecf3b33ffbb462de365a5c87
Furkan Coskun <furkancoskunn96@gmail.com>
2022-04-24 18:53:34 +0300

    Users, DataAnnotations eklendi

--- Entity/Concrete/Users.cs
+++ Entity/Concrete/Users.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using Entity.Abstract;
 using Entity.Attribute;
 
@@ -7,12 +8,22 @@ namespace Entity.Concrete
     [Class(Name = "[dbo].[Users]")]
     public class Users : IEntity
     {
-        [Key]
+        [Attribute.Key]
         public int USER_ID { get; set; }
+
+        [Required(ErrorMessage = "İsim alanı boş geçilemez")]
+        [StringLength(150, ErrorMessage = "Lütfen geçerli bir kullanıcı ismi giriniz.")]
         public string USER_NAME { get; set; }
+
+        [Required(ErrorMessage = "Şifre alanı boş geçilemez")]
         public string USER_PASSWORD { get; set; }
+
+        [Required(ErrorMessage = "Email alanı boş geçilemez")]
+        [EmailAddress(ErrorMessage = "Geçersiz email girişi!")]
         public string USER_EMAIL { get; set; }
+
         public DateTime USER_REGISTER_DATE { get; set; }
+
         public bool USER_IS_ACTIVE { get; set; }
-    }
+    } 
 }
