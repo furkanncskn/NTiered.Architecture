e2b5b6e6b9e7b557586d917f09f88dbeedd6b797
Furkan Coskun <furkancoskunn96@gmail.com>
2022-04-27 18:48:32 +0300

    Update metodu yazıldı, Add metodu güncellendi

--- SS.BusinessLogicLayer/Commen/Concrete/BaseBBL.cs
+++ SS.BusinessLogicLayer/Commen/Concrete/BaseBBL.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Reflection;
-
 using Entity.Attribute;
 using SS.DataAccessLayer.Concrete;
+using System.Linq;
 
 namespace SS.BusinessLogicLayer.Commen
 {
@@ -93,39 +93,30 @@ namespace SS.BusinessLogicLayer.Commen
 
                 string name = type.GetAttributeValue((TableAttribute info) => info.Name);
 
-                SqlCommand command = DBProvider.DB.CreateCommand(
+                using (SqlCommand command = DBProvider.DB.CreateCommand(
                         CommandType.StoredProcedure,
                         String.Format("sp_insert_{0}", name)
-                   );
-
-                PropertyInfo[] properties = type.GetProperties();
-                foreach (PropertyInfo property in properties)
+                   ))
                 {
-                    Type proType = property.GetType();
-
-                    if (Nullable.GetUnderlyingType(property.PropertyType) != null) // nullable check
+                    PropertyInfo[] properties = type.GetProperties();
+                    foreach (PropertyInfo property in properties)
                     {
-                        if (property.GetValue(entity) == null)
+                        Type proType = property.GetType();
+
+                        object value = property.GetValue(entity);
+
+                        if (property.IsThereAnAttribute(typeof(KeyAttribute)) == true) // primary key check
                         {
                             continue;
                         }
+
+                        SqlDbType propertyType = Entity.Util.GetDbType(property.PropertyType);
+
+                        command.AddSqlParameter(property.Name, ParameterDirection.Input, propertyType, value ?? DBNull.Value);
                     }
 
-                    object value = property.GetValue(entity);
-                    if (value == null)
-                    {
-                        continue;
-                    }
-
-                    if (property.IsThereAnAttribute(typeof(KeyAttribute)) == true) // key check
-                    {
-                        continue;
-                    }
-
-                    command.AddSqlParameter(property.Name, ParameterDirection.Input, value);
+                    return DBProvider.DB.NonQueryCommand(command, DBProvider.connectionString) > 0;
                 }
-
-                return DBProvider.DB.NonQueryCommand(command, DBProvider.connectionString) > 0;
             }
             catch
             {
@@ -135,9 +126,39 @@ namespace SS.BusinessLogicLayer.Commen
         
         public bool Update(T entity)
         {
-            throw new NotImplementedException();
+            Type type = typeof(T);
+
+            string name = type.GetAttributeValue((TableAttribute info) => info.Name);
+
+            PropertyInfo[] properties = type.GetProperties();
+
+            if (properties.Length == 0) { return false; }
+
+            using (SqlCommand command = DBProvider.DB.CreateCommand(
+                        type: CommandType.StoredProcedure,
+                        commandText: string.Format("sp_update_{0}_by_id", name
+                    )))
+            {
+                Type[] propertyTypes = properties.Select(p => p.PropertyType).ToArray();
+
+                object[] propertyValues = properties.Select(p => p.GetValue(entity)).ToArray();
+
+                for (int i = 0; i < propertyValues.Length; i++)
+                {
+                    DBCommand.AddSqlParameter(
+                            command, properties[i].Name, 
+                            ParameterDirection.Input, 
+                            Entity.Util.GetDbType(propertyTypes[i]),
+                            propertyValues[i] ?? DBNull.Value
+                        );
+                }
+
+                if (!(DBProvider.DB.NonQueryCommand(command, DBProvider.connectionString) > 0)) { return false; }
+            }
+            
+            return true;
         }
-        
+
         public bool DeleteAll()
         {
             throw new NotImplementedException();
