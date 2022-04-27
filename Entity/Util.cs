8de9cb730dd4bdb15ac04f95be87f2134e036340
Furkan Coskun <furkancoskunn96@gmail.com>
2022-04-27 18:48:04 +0300

    System Data Type To Sql Data Type

--- Entity/Util.cs
+++ Entity/Util.cs
@@ -1,6 +1,9 @@
 ﻿using Entity.Concrete;
 using Entity.Validations;
 using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace Entity
@@ -21,6 +24,50 @@ namespace Entity
             {
                 return "";
             }
+        }
+
+        private static Dictionary<Type, SqlDbType> typeMap;
+
+        // Create and populate the dictionary in the static constructor
+        static Util()
+        {
+            typeMap = new Dictionary<Type, SqlDbType>();
+
+            typeMap[typeof(string)] = SqlDbType.NVarChar;
+            typeMap[typeof(char[])] = SqlDbType.NVarChar;
+            typeMap[typeof(byte)] = SqlDbType.TinyInt;
+            typeMap[typeof(short)] = SqlDbType.SmallInt;
+            typeMap[typeof(int)] = SqlDbType.Int;
+            typeMap[typeof(long)] = SqlDbType.BigInt;
+            typeMap[typeof(byte[])] = SqlDbType.Image;
+            typeMap[typeof(bool)] = SqlDbType.Bit;
+            typeMap[typeof(DateTime)] = SqlDbType.DateTime2;
+            typeMap[typeof(DateTimeOffset)] = SqlDbType.DateTimeOffset;
+            typeMap[typeof(decimal)] = SqlDbType.Money;
+            typeMap[typeof(float)] = SqlDbType.Real;
+            typeMap[typeof(double)] = SqlDbType.Float;
+            typeMap[typeof(TimeSpan)] = SqlDbType.Time;
+            /* ... and so on ... */
+        }
+
+        // Non-generic argument-based method
+        public static SqlDbType GetDbType(Type giveType)
+        {
+            // Allow nullable types to be handled
+            giveType = Nullable.GetUnderlyingType(giveType) ?? giveType;
+
+            if (typeMap.ContainsKey(giveType))
+            {
+                return typeMap[giveType];
+            }
+
+            throw new ArgumentException($"{giveType.FullName} is not a supported .NET class");
+        }
+
+        // Generic version
+        public static SqlDbType GetDbType<T>()
+        {
+            return GetDbType(typeof(T));
         }
     }
 }
